f7b2b0477701ed2c103097856febae7fad5f966d
Jakub1994 <ostach2094@gmail.com>
2021-03-17 17:03:31 +0000

    connect to mangodb

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
