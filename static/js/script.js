cd9b2065d5f30600df96773c2c41f7f64559d9a9
Jakub1994 <ostach2094@gmail.com>
2021-03-23 19:23:41 +0000

    tooltipe

--- static/js/script.js
+++ static/js/script.js
@@ -6,3 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {inDuration: "200"});
   });
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems, {enterDelay: "100"});
+  });
