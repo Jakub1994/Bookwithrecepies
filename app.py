130dc13430e7c3d649f379e96b5bdf657ed96df8
Jakub <64478192+Jakub1994@users.noreply.github.com>
2021-04-14 10:22:57 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
 from flask import (
     Flask, flash, render_template,
-    redirect, request, session, url_for)
+    redirect, request, session, templating, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from pymongo import MongoClient
 from werkzeug.security import generate_password_hash, check_password_hash
-from PIL import Image
+import gridfs
 if os.path.exists("env.py"):
     import env
 
@@ -22,31 +23,31 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/recipes")
 def get_recipe():
-    recipes = list(mongo.db.recipies.find())
-    return render_template("recipes.html", recipes=recipes)
+    recipies = list(mongo.db.recipies.find())
+    return render_template("recipes.html", recipies=recipies)
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        # check if username already exist in db
+        # check if username already exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
-        
+
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
 
         register = {
-        "username": request.form.get("username").lower(),
-        "password": generate_password_hash(request.form.get("password"))
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
         }
         mongo.db.users.insert_one(register)
 
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
-        flash("Registration Successful")
-        return redirect(
-            url_for("profile", username=session["user"]))
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
@@ -84,11 +85,11 @@ def myProfile(username):
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    
     if session["user"]:
         return render_template("myProfile.html", username=username)
 
     return redirect(url_for("login"))
+
 
 @app.route("/logout")
 def logout():
@@ -104,20 +105,50 @@ def new_recipes():
         vegan = "on" if request.form.get("vegan") else "off"
         recipe = {
             "recipe_name": request.form.get("recipe_name"),
+            "recipe_image": request.form.get("recipe_image"),
             "short_describe": request.form.get("short_describe"),
-            "ingredients": request.form.get("ingridients"),
+            "ingredients": request.form.get("ingredients"),
             "step_1_of_preparation": request.form.get("step_1_of_preparation"),
             "step_2": request.form.get("step_2"),
             "step_3": request.form.get("step_3"),
             "vegan": vegan,
-            "created_by": session["user"]
+            "created_by": session["user"],
+            "recipe_url": request.form.get("recipe_name")
         }
         mongo.db.recipies.insert_one(recipe)
-        flash("Task Successfully Added")
-        return redirect(url_for("get_recipe"))
+        flash("Recipe Added")
+        return redirect(url_for("new_recipes"))
+    return render_template("new_recipes.html", page_title="Add a Recipe")
 
-    prep_time = mongo.db.prep_time.find().sort("cooking_time", 1)
-    return render_template("new_recipes.html", prep_time=prep_time)
+
+@app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
+def edit_recipe(recipe_id):
+    if request.method == "POST":
+        vegan = "on" if request.form.get("vegan") else "off"
+        submit = {
+            "recipe_name": request.form.get("recipe_name"),
+            "recipe_image": request.form.get("recipe_image"),
+            "short_describe": request.form.get("short_describe"),
+            "ingredients": request.form.get("ingredients"),
+            "step_1_of_preparation": request.form.get("step_1_of_preparation"),
+            "step_2": request.form.get("step_2"),
+            "step_3": request.form.get("step_3"),
+            "vegan": vegan,
+            "created_by": session["user"],
+            "recipe_url": request.form.get("recipe_name")
+        }
+        mongo.db.recipies.update({"_id": ObjectId(recipe_id)}, submit)
+        flash("Recipe Updated")
+
+    recipe = mongo.db.recipies.find_one({"_id": ObjectId(recipe_id)})
+    return render_template("edit_recipe.html", recipe=recipe)
+
+
+@app.route("/delete_recipe/<recipe_id>")
+def delete_recipe(recipe_id):
+    mongo.db.recipies.remove({"_id": ObjectId(recipe_id)})
+    flash("Successfully Deleted")
+    return redirect(url_for("get_recipe"))
 
 
 if __name__ == "__main__":
--- static/js/script.js
+++ static/js/script.js
@@ -13,8 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Tooltip.init(elems, {enterDelay: "100"});
   });
 
-  $('#recipe_name, #preparation_time, #difficulty_level, #short_describe, #ingredients, #step_1_of_preparation, #step_2, #step_3').val('');
-  M.textareaAutoResize($('#recipe_name, #preparation_time, #difficulty_level, #short_describe, #ingredients, #step_1_of_preparation, #step_2, #step_3'));
+  
 
    document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
